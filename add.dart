06ffcb184962d7f9765de40a7dbcd5c440e59ad7
Akshay Mohan <akshmo.95@gmail.com>
2023-12-10 14:29:48 +0530

    whohooo

--- add.dart
+++ add.dart
@@ -1,7 +1,14 @@
 void main() {
   int a = 1;
-int b = 2;
+  int b = 2;
 
-print(a + b);
-
+  print(a + b);
+  print("Whohooo!");
+    print("Whohooo!");
+      print("Whohooo!");
+        print("Whohooo!");
+          print("Whohooo!");
+            print("Whohooo!");
+              print("Whohooo!");
+                print("Whohooo!");
 }
