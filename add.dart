7daa9860a21ff848b949ad6f72d2b5728bed7b63
Akshay Mohan <akshmo.95@gmail.com>
2023-12-11 06:57:49 +0530

    Switched to multiplication

--- add.dart
+++ add.dart
@@ -1,18 +1,9 @@
 void main() {
-<<<<<<< HEAD
-  int a = 15;
-=======
-  int a = 22;
->>>>>>> d08c112e9361c7b238ccc449182f868fb9a55d99
+
+  int a = 20;
+
   int b = 2;
 
-  print(a + b);
-  print("Whohooo!");
-    print("Whohooo!");
-      print("Whohooo!");
-        print("Whohooo!");
-          print("Whohooo!");
-            print("Whohooo!");
-              print("Whohooo!");
-                print("Whohooo!");
+  print(a * b);
+
 }
